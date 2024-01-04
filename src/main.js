dfb304718b768768900cde9ae1c38e781fe281c5
Shubham Kumar <shubham-work@outlook.com>
2024-01-04 23:28:32 +0530

    3D logo

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/ZuUagw6-AcmKBO8b/scene.splinecode');
